dc9a498e6d38ae1b70faf05a1431a89efabda35a
Reed Gantz <reedm121@gmail.com>
2024-07-16 11:43:46 -0400

    Initial Commit

---
+++ Fetch_challenge/Fetch_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_challengeApp.swift
+//  Fetch_challenge
+//
+//  Created by Reed Gantz on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
